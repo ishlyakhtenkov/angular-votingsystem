a93d2edd10fb553b7bf2160c7c47826f7f0e9bc2
igar15 <igar15@yandex.ru>
2021-05-03 21:38:07 +0300

    refactor menu component

--- src/app/components/menu-today/menu-today.component.ts
+++ src/app/components/menu-today/menu-today.component.ts
@@ -13,7 +13,8 @@ export class MenuTodayComponent implements OnInit {
 
   menu: Menu = new Menu();
 
-  // newDishes: Dish[] = [];
+  currentDishes: Dish[] = [];
+
   isUpdated: boolean = false;
 
   @Input()
@@ -31,6 +32,7 @@ export class MenuTodayComponent implements OnInit {
     this.restaurantService.getMenuToday(+this.restaurantId).subscribe(
       data => {
         this.menu = data;
+        this.currentDishes = Array.from(this.menu.dishes);
         this.restaurantService.showVoteButton(this.menu.dishes.length > 0);
       }
     );
@@ -44,25 +46,41 @@ export class MenuTodayComponent implements OnInit {
     let dish: Dish = new Dish();
     dish.name = "sapogi";
     dish.price = 200;
-    this.menu.dishes.push(dish);
-    this.isUpdated = true;
-    this.restaurantService.showVoteButton(this.menu.dishes.length > 0);
+    // this.menu.dishes.push(dish);
+    this.currentDishes.push(dish);
+    // this.isUpdated = true;
+    // this.restaurantService.showVoteButton(this.menu.dishes.length > 0);
+    this.restaurantService.showVoteButton(this.currentDishes.length > 0); //may be we should show button on real dishes (menu.dishes)
   }
 
   deleteDish(dishName: string) {
     console.log("delete dish with name: " + dishName);
-    let index = this.menu.dishes.findIndex(tempDish => tempDish.name === dishName);
+    // let index = this.menu.dishes.findIndex(tempDish => tempDish.name === dishName);
+    let index = this.currentDishes.findIndex(tempDish => tempDish.name === dishName);
     console.log("delete dish with index: " + index);
-    this.menu.dishes.splice(index, 1);
-    this.isUpdated = true;
-    this.restaurantService.showVoteButton(this.menu.dishes.length > 0);
-    for (let dish of this.menu.dishes) {
-      console.log("dish name " + dish.name);
-    }
+    // this.menu.dishes.splice(index, 1);
+    // this.isUpdated = true;
+    this.currentDishes.splice(index, 1);
+    // this.restaurantService.showVoteButton(this.menu.dishes.length > 0);
+    this.restaurantService.showVoteButton(this.currentDishes.length > 0); //may be we should show button on real dishes (menu.dishes)
+    // for (let dish of this.menu.dishes) {
+    //   console.log("dish name " + dish.name);
+    // }
   }
 
   publish() {
-    console.log("Menu has been published");
+        // TODO pass currentDishes to backend
+    if (this.menu.dishes.length == 0) {
+      console.log("MenuTo POST for restaurant: " + this.restaurantId);
+    } else if (this.currentDishes.length == 0) {
+      console.log("MenuTo DELETE for restaurant: " + this.restaurantId);
+    } else {
+      console.log("MenuTo PUT for restaurant: " + this.restaurantId);
+    }
     window.location.reload();
+  }
+
+  isEqual(): boolean {
+    return JSON.stringify(this.currentDishes) === JSON.stringify(this.menu.dishes);
   }
 }
