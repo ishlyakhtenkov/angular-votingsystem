900e3bc1f6ee3d6d50fafe115efc24b63accf84a
igar15 <igar15@yandex.ru>
2021-05-07 23:21:50 +0300

    add register functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   constructor(private router: Router) {
     router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
-        if (event['url'] == '/login') {
+        if (event['url'] == '/login' || event['url'] == '/register') {
           this.showHeader = false;
           this.showSearch = false;
         } else {
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/common/user';
+import { UserTo } from 'src/app/common/user-to';
+import { NotificationType } from 'src/app/enum/notification-type.enum';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  showLoading: boolean;
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute, private authenticationService: AuthenticationService, private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/restaurants');
+    }
   }
 
+  onRegister(registerTo: UserTo): void {
+    let userTo = new UserTo(registerTo.id, registerTo.name, registerTo.email, registerTo.password);
+    console.log(userTo);
+    this.showLoading = true;
+    this.subscriptions.push(
+      this.authenticationService.register(userTo).subscribe(
+        (response: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `A new account was created for ${response.email}`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          console.log(errorResponse);
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.details);
+          this.showLoading = false;
+        }
+      )
+    );
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string) {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'An error occured. Please try again');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());   
+  }
 }
